3121ad6cec1915b1c81533038d3129d30a18f101
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-04 22:34:31 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist={
+  Nicole:"I Love You"
+} 
+
+function updatePlaylist(playlist,artist,title)
+{
+  playlist.artist:"title"
+  return playlist
+}
+
+function removeFromPlaylist(playlist,artist)
+{
+  delete playlist.artist
+  return playlist
+}
